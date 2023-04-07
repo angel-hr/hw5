066d14b21acb50a16145db064d7244dbbfefeea8
Angel Hernandez Ramirez <afhernan@usc.edu>
2023-04-07 21:40:57 +0000

    second

--- schedwork.cpp
+++ schedwork.cpp
@@ -16,11 +16,23 @@ using namespace std;
 // a constant that can be used to indicate an INVALID 
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
-static const Worker_T INVALID_ID = (unsigned int)-1;
+//static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
 
+bool helper(const AvailabilityMatrix& a, const size_t d, 
+            const size_t m, DailySchedule& s, size_t& row);
+/*
+bool helper(const AvailabilityMatrix& a, DailySchedule& sched,
+            const size_t d, size_t col, size_t row, const size_t m);
+*/
+
+bool scheduleBuilder(DailySchedule& s, size_t& row, size_t& col,
+                    const size_t d, const size_t m); //row corresponds to the day, and column corresponds to worker
+
+bool valid(Worker_T worker, const DailySchedule& sched, const size_t d,
+           const size_t m, size_t& count, size_t &row);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,9 +48,111 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    for (size_t i =0; i < avail.size(); i++){  //allocate row to sched for the days
+        vector<Worker_T> day;
+        sched.push_back(day);
+    }
 
+    
 
-
-
+    //return helper(avail, sched, dailyNeed, 0, 0, maxShifts);
+    size_t row = 0;
+    return helper(avail, dailyNeed, maxShifts, sched, row);  
 }
 
+/*
+bool helper(const AvailabilityMatrix& a, DailySchedule& sched,
+            const size_t d, size_t col, size_t row, const size_t m)
+{
+    if (row == a.size()) {return true;}
+    if (col == a[row].size()) { //if we move to a stack where the col == d then we can go the next row
+        //clear the row, and return false
+        if (sched[row].size() < d){
+            sched[row].clear();
+        }
+        else {
+            if (col != 0){
+                
+            }
+        }
+        return false;
+    } 
+
+    if (a[row][col] == true){
+        bool fit = scheduleBuilder(sched, row, col, d, m);  
+        if (fit){
+            if (sched[row].size() == d){
+                return helper(a,sched, d, 0, row+1, m);
+            }
+            
+        }
+    }
+
+    return helper(a, sched, d, col +1, row, m);
+}
+*/
+
+
+bool helper(const AvailabilityMatrix& a, const size_t d, 
+            const size_t m, DailySchedule& s, size_t& row)
+{
+    if (row == a.size()){return true;} 
+
+    unsigned int d_count = 0;
+    for (size_t col = 0; col < a[row].size(); col++){ // one loop recursive search
+        if (a[row][col] == true){
+            bool fit = scheduleBuilder(s, row, col, d, m); 
+            if (fit){d_count++;}
+            if (d_count == d){
+                size_t row2 = row + 1;
+                bool status = helper(a, d, m, s, row2);
+                if (status){return true;} //if false continue
+                else {
+                    d_count = 0;
+                    //s[row].pop_back();
+                    s[row].clear();
+                    if (col  != 0){
+                       --col; //go back a column
+                    }
+                    //return false;
+                }
+            }
+        }
+    }
+
+    s[row].clear();
+    return false; //if we go through the entire column without filling the nxd sched row then we don't have a valid solution
+}
+
+
+bool scheduleBuilder(DailySchedule& sched, size_t& row, size_t& col, 
+                    const size_t d, const size_t m)
+{  
+    if (sched[row].size() == d) {return false;} //this means that all columns in the sched matrix are filled so we were not able to put a new woker in the row
+
+    Worker_T worker = col;
+    sched[row].push_back(worker);
+    size_t count =0, row2 =0;
+    bool okay = valid(worker, sched, d, m, count, row2); //check if valid, start at the first row
+    if (!okay){
+        sched[row].pop_back();
+        //sched[row].clear();
+        return false;
+    }
+    return true;
+}
+
+
+bool valid(Worker_T worker, const DailySchedule& sched, const size_t d,
+           const size_t m, size_t& count, size_t &row)
+{
+    if (count > m){return false;}
+    if (row == sched.size()) {return true;}
+
+    for (size_t i =0; i < sched[row].size(); i++){
+        if (sched[row][i] == worker){
+            return valid(worker, sched, d, m, ++count, ++row); //there can only be one instance of a worker in each row
+        }
+    }
+    return valid(worker, sched, d, m, count, ++row);    //if we traverse the row then move to the next row
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,16 +13,178 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+/*void arrange(std::set<std::string>& jumbles, const std::string& in,
+             const std::string& floating, size_t pos1, size_t pos2);
+*/
+void arrange(std::set<std::string>& jumbles, const std::string& in,
+             const std::string& floating, size_t pos);
 
+void fill(std::set<std::string>& words, const std::string& jumbled, 
+          char letter, size_t step);
+
+bool triple(const std::string& in, const std::string& floating, size_t pos1, size_t pos2);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
+    const std::set<std::string>& dict){   
+    std::set<std::string> jumbled; //create an empty set
 
+    arrange(jumbled, in, floating, 0);
+
+    std::set<std::string> words;
+    std::set<std::string>::iterator it;
+    for (it = jumbled.begin(); it != jumbled.end(); ++it){
+        if (dict.find(*it) != dict.end()){
+            words.insert(*it); 
+        }
+    }
+
+    return words;
 }
 
 // Define any helper functions here
+void arrange(std::set<std::string>& jumbles, const std::string& in,
+             const std::string& floating, size_t pos)
+{
+    //first loop to set charatcer in any given location
+    if (pos == floating.size()) {
+        std::string word = in; //deconvert in from const
+        fill(jumbles, word, 'a', 0);
+        return;
+    }
+
+    for (size_t i = 0; i < in.size(); i++){
+        std::string temp(in);
+        if (in[i] == '-'){
+            bool check = triple(in, floating, i, pos);
+            if (check){ 
+                temp[i] = floating[pos];
+                arrange(jumbles, temp, floating, ++pos); //increment the size of floating[pos] so that we can get to the next letter
+                --pos;
+            }
+        }
+    }
+}
+
+bool triple(const std::string& in, const std::string& floating, size_t pos1, size_t pos2){
+    if (pos1 > 1){ // aa-a
+        if (in[pos1-1] == floating[pos2] && in[pos1 -2] == floating[pos2]){ //then adding floating[pos2] to in[pos1] would cause a triple
+            return false;
+        }
+    } 
+
+    if (pos1 > 0 && in.size() > pos1 + 1) { // a-a or aa-a
+        if (in[pos1-1] == floating[pos2] && in[pos1+1] == floating[pos2]){
+            return false;
+        }
+    }
+
+    if (in.size() > pos1 +2){ //  -aa or a-aa or aa-aa
+        if(in[pos1+1] == floating[pos2] && in[pos1 +2] == floating[pos2]){
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+void arrange(std::set<std::string>& jumbles, const std::string& in,
+             const std::string& floating, size_t pos1, size_t pos2)
+{
+    if (pos1 == in.size()-1) {return;}
+    if (pos2 == floating.size()){
+        std::string word = in; //deconvert in from const
+        fill(jumbles, word, 'a', 0);
+        return;
+    }
+    
+    arrange(jumbles, in, floating, ++pos1, pos2);
+    if (in[pos1] != '-') {return;}
+    
+    //check for triple formations
+    if (pos1 > 1){ // aa-a
+        if (in[pos1-1] == floating[pos2] && in[pos1 -2] == floating[pos2]){ //then adding floating[pos2] to in[pos1] would cause a triple
+            arrange(jumbles, in, floating, pos1, ++pos2); //move to the next location in the floating string
+        }
+    } 
+
+    if (pos1 > 0 && in.size() > pos1 + 1) { // a-a or aa-a
+        if (in[pos1-1] == floating[pos2] && in[pos1+1] == floating[pos2]){
+            arrange(jumbles, in, floating, pos1, ++pos2);
+        }
+    }
+
+    if (in.size() > pos1 +2){ //  -aa or a-aa or aa-aa
+        if(in[pos1+1] == floating[pos2] && in[pos1 +2] == floating[pos2]){
+            arrange(jumbles, in, floating, pos1, ++pos2);
+        }
+    }
+
+
+    std::string blah(in);   //make a copy of in because its const
+    blah[pos1] = floating[pos2];   //replace the '-' with the letter at floating[pos2]
+
+    arrange(jumbles, blah, floating, pos1, ++pos2);
+}
+*/
+
+void fill(std::set<std::string>& words, const std::string& jumbled, char letter, size_t step) //there exists no three letter repetitions
+{
+    if (letter > 122){return;}  //if the charachter is greater than lowercase z
+    if (jumbled.size() == step) { //these will take care of the empty case and when we reach the limit
+        if (jumbled.size() > 0){
+            words.insert(jumbled); //if we reach the end of the word then jumbled word has been filled
+        }
+        return;
+    } 
+
+    /*if (jumbled[step] == 'q'){ //q is always followed by u
+        if (step+1 < jumbled.size()){
+            std::string word(jumbled);
+            word[step + 1] = 'u';
+            fill(words, word, 'a', ++step);  
+        }
+    }*/
+
+    if (jumbled.size() < 3){ //we dont have to worry about repetitons
+        if (jumbled[step] != '-') {
+            fill(words, jumbled, letter, ++step);
+        }
+        std::string word(jumbled);
+        word[step] = letter;
+        fill(words, jumbled, ++letter, step); //this will insert all the words: a, b, c, d ...  
+        fill(words, word, 'a', step++);
+    }
+    else { //if jumble size is 3 or greater than we need to avoid three letter repetitions
+        if (jumbled[step] != '-'){
+            fill(words, jumbled, letter, ++step);
+        }
+        else {
+            if (step == 0){ //case -nn ==> inn or zoo
+                if (jumbled[step+1] == letter && jumbled[step+2] == letter){      //check two letters foward
+                    letter++;  //increase the letter if it would make a triple
+                }
+            }
+            else {  
+                if (step+1 < jumbled.size()){
+                    if (jumbled[step-1] == letter && jumbled[step+1] == letter){//check one back and one front
+                        letter++;
+                    } 
+                }
+                if (step + 2 < jumbled.size()){ //check if the two up front make a triple
+                    if (jumbled[step +1] == letter && jumbled[step+2] == letter){
+                        letter++;
+                    }
+                }
+            }       
+
+            std::string word(jumbled);
+            word[step] = letter;
+            fill(words, jumbled, ++letter, step);
+            fill(words, word, 'a', ++step);
+        }
+    }
+}
+
